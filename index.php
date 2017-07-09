a46a8b8fac70b897998ae4a5f41410454268e48e
vyanktesh-sutaria <vsutaria72@gmail.com>
2017-07-09 13:52:32 +0530

    Testing

--- index.php
+++ index.php
@@ -5,4 +5,9 @@ require "autoload.php";
 
 usr Abraham\TwitterOAuth\TwitterOAuth;
 
+define('CONSUMER_KEY', 'NvrtYx5XKgHiGKROJBpA8i921');
+define('CONSUMER_SECRET', 'PmtPCnSaZamJfqzBkz46O7pfGcEIetVxzJVtZ2VdZLDCZW3dDH');
+//define('OAUTH_CALLBACK', value);
+
+echo "hello World";
 ?>
