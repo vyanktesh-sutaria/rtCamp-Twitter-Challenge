73e4ec6043192c26f6ceca991ec494c2865d5171
vyanktesh-sutaria <vsutaria72@gmail.com>
2017-07-09 15:00:09 +0530

    Callback

--- index.php
+++ index.php
@@ -3,11 +3,29 @@
 session_start();
 require "autoload.php";
 
-usr Abraham\TwitterOAuth\TwitterOAuth;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 define('CONSUMER_KEY', 'NvrtYx5XKgHiGKROJBpA8i921');
 define('CONSUMER_SECRET', 'PmtPCnSaZamJfqzBkz46O7pfGcEIetVxzJVtZ2VdZLDCZW3dDH');
 //define('OAUTH_CALLBACK', value);
 
-echo "hello World";
+//echo "hello World";
+
+if(!isset($_SESSION['access_token']))
+{
+	$connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token');
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url=$connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token'] ));
+	echo $url;
+}
+else
+{
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,$access_token['oauth_token'],$access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+	echo $user->status->text;
+}
+
 ?>
